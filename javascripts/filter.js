b0301fbb21bfdafa291a9216f2d8f00c5c57a563
Ashley Chapokas <achapokas@weblinc.com>
2018-04-02 08:01:57 -0700

    Begins to refactor JS, adds a DOMContentLoaded event listener. Checking functionality

--- javascripts/filter.js
+++ javascripts/filter.js
@@ -1,5 +1,3 @@
-
-
 const request = function(){
   const bytes = new Request('https://achapokas.github.io/pivot-table/javascripts/bytes.json');
 
@@ -11,7 +9,7 @@ const request = function(){
       return 'Request failed to complete'
     })
     .then(function(json){
-      insertTbody(json)
+      insertTable(json)
     });
 },
 
@@ -30,94 +28,154 @@ seedTable = function(results) {
   return template
 },
 
-clone = function(address, event) {
-  var target = event.target,
-      targetText = target.innerHTML;
+cloneValue = function(address, event) {
+  const target = event.target,
+        targetText = target.innerHTML;
 
    address.setAttribute("value", targetText);
 },
 
-sortRows = function(td, tr, inputValue, i) {
-  if (td) {
-    if(inputValue === td.innerHTML) {
-      tr.style.display = ""
+filterRows = function(data, row, value) {
+  if (data) {
+    if(value === data.innerHTML) {
+      row.style.display = ""
     } else {
-      tr.style.display = "none"
+      row.style.display = "none"
     }
   }
-}
+},
 
-setsFilter = function(select, address) {
-  const selectIndex = select.options[select.selectedIndex].text,
-        inputValue = address.value,
-        tr = document.getElementsByTagName("tr");
+storesFilterParams = function(table, select, address) {
+  const index = select.options[select.selectedIndex].text,
+        value = address.value,
+        row = table.getElementsByTagName("tr");
 
-    if ( selectIndex === "Destination" ) {
-      for(var i = 0; i < tr.length; i++) {
-        var td = tr[i].getElementsByTagName("td")[0];
+    if ( index === "Destination" ) {
+      for(let i = 0; i < row.length; i++) {
+        var data = row[i].getElementsByTagName("td")[0];
 
-        sortRows(td, tr[i], inputValue, i)
+        filterRows(data, row[i], value, i)
       }
     } else {
-      for(var i = 0; i < tr.length; i++) {
-          var td = tr[i].getElementsByTagName("td")[1];
+      for(let i = 0; i < row.length; i++) {
+          var data = row[i].getElementsByTagName("td")[1];
 
-          sortRows(td, tr[i], inputValue, i)
+        filterRows(data, row[i], value, i)
       }
     }
+},
+
+removesFilterParams = function(tr) {
+  for (let i = 0; i < tr.length; i++) {
+    tr[i].removeAttribute("style")
+  }
+},
+
+sessionFilterParams = function() {},
+
+filterTemplate = function(select, address) {
+  const index = select.options[select.selectedIndex].text,
+        value = address.value,
+        aside = document.querySelector("aside");
+        asideLinks = `<a href="#" data-option="${index}">${value}</a>`;
+
+  aside.insertAdjacentHTML('beforeEnd', asideLinks);
 }
 
-insertTbody = function(json) {
-  const table = document.getElementById("pivot"),
-        thead = document.getElementById("thead"),
-        form = document.forms["filter"],
-        address = form.elements["address"],
-        select = form.elements["filter-by"],
-        reset = form.elements["reset"],
-        tr = document.getElementsByTagName("tr");
+test = function(tr) {
+  for(var i = 0; i < tr.length; i++) {
+    var style = sessionStorage.getItem('style' + [i]);
 
-  var results = [],
-      dests = [];
+    tr[i].style.display = style
+  }
+},
+
+init = function() {
+const table = document.getElementById("pivot"),
+      tr = table.getElementsByTagName("tr"),
+      form = document.forms["filter"],
+      address = form.elements["address"],
+      select = form.elements["filter-by"],
+      reset = form.elements["reset"];
+
+  table.addEventListener("click", function(event){ cloneValue( address, event) })
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    storesFilterParams(table, select, address)
+    filterTemplate(select, address)
+  })
+
+  reset.addEventListener("click", function(event){ event.preventDefault(); removesFilterParams(tr) })
+
+
+  window.addEventListener("beforeunload", function (event) {
+    event.preventDefault();
+
+    for(var i = 0; i < tr.length; i++) {
+      sessionStorage.setItem('style' + [i], tr[i].style.display)
+    }
+  });
+
+  window.addEventListener("DOMContentLoaded", test(tr), false);
+
+  // window.onload = function() {
+  //   for(var i = 0; i < tr.length; i++) {
+  //     var style = sessionStorage.getItem('style' + [i]);
+
+  //     tr[i].style.display = style
+  //   }
+  // }
+},
+
+recentFilters = function() {
+  var targetNode = document.getElementById('aside');
+  var config = { childList: true };
+
+  var callback = function(mutationsList) {
+      for(var mutation of mutationsList) {
+          if (mutation.type == 'childList') {
+            var childNodes = targetNode.querySelectorAll('a'),
+            table = document.getElementById("pivot"),
+            form = document.forms["filter"],
+            address = form.elements["address"],
+            select = form.elements["filter-by"];
+
+
+            targetNode.addEventListener('click', function(event){
+              event.preventDefault();
+              const target = event.target,
+                    targetText = target.innerHTML,
+                    targetAttribute = target.getAttribute("data-option");
+
+                    cloneValue(address, event)
+
+                    storesFilterParams(table, select, address)
+              })
+          }
+      }
+  };
+
+  var observer = new MutationObserver(callback);
+
+  observer.observe(targetNode, config);
+},
+
+insertTable = function(json) {
+  const table = document.getElementById("pivot");
+
+  var results = [];
 
   for(var i = 0; i < json.length; i++){
     const result = json[i].result,
           resultDest = result["All_Traffic.dest"]
 
           results.push(result)
-          dests.push(resultDest)
   }
 
-  thead.insertAdjacentHTML('afterEnd', seedTable(results));
-
-  table.addEventListener("click", function(event){ clone(address, event) })
-
-  form.addEventListener("submit", function(event) { event.preventDefault(); setsFilter(select, address)})
-
-  reset.addEventListener("click", function(event){
-    event.preventDefault();
-    for (var i = 0; i < tr.length; i++) {
-      tr[i].removeAttribute("style")
-    }
-  })
-
-window.onbeforeunload = function(){
-  for(var i = 0; i < tr.length; i++) {
-    sessionStorage.setItem('style' + [i], tr[i].style.display)
-  }
-}
-
-window.onload = function() {
-  for(var i = 0; i < tr.length; i++) {
-    var test = sessionStorage.getItem('style' + [i]);
-
-    tr[i].style.display = test
-  }
-}
-
-  // Store session storage
-  // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API
-  // If input value returns empty string alert user
-  // Validation on "blur" provide a tooltip for user to include IP address
+  table.insertAdjacentHTML('beforeEnd', seedTable(results));
 }
 
 request()
+init()
+recentFilters()
